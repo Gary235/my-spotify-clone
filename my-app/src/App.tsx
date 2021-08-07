8450529d7dfee39610ac600db94d19b19bddc711
Gary Berkman <gary.berkman@wearepsh.com>
2021-08-06 21:03:43 -0300

    create project and start ts/lint/ i18n configs

---
+++ my-app/src/App.tsx
@@ -0,0 +1,14 @@
+import React, {FunctionComponent} from 'react';
+
+import './styles.css';
+
+const App: FunctionComponent = () => {
+
+  return (
+    <div className="App">
+      Hello World!
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/configs/i18n.ts
@@ -0,0 +1,14 @@
+import i18next from 'i18next';
+
+export const locales = {
+  EN: 'en-US',
+  ES: 'es-AR',
+};
+
+const DEFAULT_LOCALE = locales.ES;
+
+i18next.init({
+  // initImmediate: false,
+  fallbackLng: DEFAULT_LOCALE,
+  lng: DEFAULT_LOCALE,
+});
